9e6bd02e71a07c2a36ce7e8d92889d5ad2a1ecd0
zhangyanhua <939593210@qq.com>
2022-05-20 23:12:30 +0800

    feat:540

---
+++ src/ts-code/540有序的有效数组.js
@@ -0,0 +1,35 @@
+var nums = [1, 1, 2, 3, 3, 4, 4, 8, 8];
+var singleNonDuplicate = function (nums) {
+    let l = 0;
+    let r = nums.length - 1;
+    while (l <= r) {
+
+        let mid = l + ((r - l) >> 1)
+        // 如果中间点相等，则说明是重复的，那么只能向右边找
+        if (nums[mid] === nums[mid + 1]) {
+            if (mid % 2 === 0) {
+                // 偶数
+                l = mid + 1
+            } else {
+                r = mid
+            }
+        } else if (nums[mid] == nums[mid - 1]) {
+            if (mid % 2 !== 0) {
+                // 偶数
+                l = mid + 1
+            } else {
+                r = mid
+            }
+
+        } else {
+            l = mid;
+            break;
+        }
+    }
+    console.log('llllllll', l, r)
+    return nums[l]
+};
+var nums2 = [3, 3, 7, 7, 10, 11, 11]
+var nums3 = [1, 1, 2]
+var nums4 = [1, 1, 2, 2, 3]
+console.log(singleNonDuplicate(nums4))
