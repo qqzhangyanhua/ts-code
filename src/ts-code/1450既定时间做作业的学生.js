45a4334d2d05a54a3ad0ba444729ea72bd42c395
zhangyanhua <939593210@qq.com>
2022-06-20 21:29:29 +0800

    feat:1450

---
+++ src/ts-code/1450既定时间做作业的学生.js
@@ -0,0 +1,10 @@
+var busyStudent = function (startTime, endTime, queryTime) {
+    let count = 0;
+    for (let i = 0; i < startTime.length; i++) {
+        if (startTime[i] <= queryTime && endTime[i] >= queryTime) {
+            count++
+        }
+    }
+    return count
+
+};
