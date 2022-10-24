c83d36c9353eb1801e8eb020afa10e68e262e9ef
zhangyanhua <939593210@qq.com>
2022-10-24 22:55:17 +0800

    feat:solid

--- 演示文件夹/src/text.js
+++ 演示文件夹/src/text.js
@@ -1,6 +1,49 @@
+const context = []
 
-
-function createSignal(value){
-
-    return [readSignal(),setter()]
+const createSignal = (value) => {
+    console.log('value: ' + value)
+    const subscriptions = new Set();
+    const readSignal = () => {
+        const running = context.pop();
+        if (running) {
+            subscriptions.add({
+                execute: running.execute
+            })
+            running.deps.add(subscriptions)
+        }
+        console.log('value: ' + value)
+        return value;
+    }
+    const setter = (newValue) => {
+        value = newValue;
+        for (const sub of [...subscriptions]) {
+            sub.execute();
+        }
+    }
+    return [readSignal, setter()]
 }
+
+
+// const createSignal = (value) => {
+//     const subscriptions = new Set();
+//     const readFn = () => {
+//       const running = context.pop();
+//       if (running) {
+//         subscriptions.add({
+//           execute: running.execute
+//         });
+//         running.deps.add(subscriptions);
+//       }
+//       return value;
+//     };
+//     const writeFn = (newValue) => {
+//       value = newValue;
+//       for (const sub of [...subscriptions]) {
+//         sub.execute();
+//       }
+//     };
+//     return [readFn, writeFn];
+//   };
+  
+const [age, setAge] = createSignal(12);
+console.log('wwwww', age())
