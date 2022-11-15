4b5811ee17644879e738f3cb653847de29a9723d
zhangyanhua <939593210@qq.com>
2022-11-15 23:03:43 +0800

    feat：斐波那契额数列

--- .leetcode/509.斐波那契数.js
+++ .leetcode/509.斐波那契数.js
@@ -9,16 +9,47 @@
  * @param {number} n
  * @return {number}
  */
-var fib = function(n) {
-// if(n<=1){
-//     return n
-// }
-// return fib(n-1)+fib(n-2)
-let dp = [0,1]
-for(let i =2;i<=n;i++){
-    dp[i] = dp[i-1]+dp[i-2]
-}
-return dp[n]
-};
-// @lc code=end
+// [0,1]
+function helper(memo, n) {
+    if (n <= 1) {
+        return n;
+    }
+    if (memo[n]) {
+        return memo[n]; //返回缓存结果
+    }
 
+
+    memo[n] = helper(memo, n - 1) + helper(memo, n - 2)
+    return memo[n];
+
+}
+var fib = function (n) {
+    //以下有多种方法
+    // if(n<=1){
+    //     return n
+    // }
+    // return fib(n-1)+fib(n-2)
+    // let dp = [0,1]
+    // for(let i =2;i<=n;i++){
+    //     dp[i] = dp[i-1]+dp[i-2]
+    // }
+    // return dp[n]
+    // let memo = [];
+    // return helper(memo, n)
+
+    if (n <= 1) {
+        return n
+    }
+    let dp1 = 0,
+        dp2 = 1
+    let dp3
+    for (let i = 2; i <= n; i++) {
+        dp3 = dp1 + dp2
+        dp1 = dp2
+        dp2 = dp3
+    }
+    return dp3
+};
+
+
+// @lc code=end
