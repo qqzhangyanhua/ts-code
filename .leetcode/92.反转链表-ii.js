938510824e1678eb255415a4f3db41ba60cf3a43
zhangyanhua <939593210@qq.com>
2022-11-19 23:34:29 +0800

    feat:反转链表

--- .leetcode/92.反转链表-ii.js
+++ .leetcode/92.反转链表-ii.js
@@ -19,6 +19,27 @@
  * @return {ListNode}
  */
 var reverseBetween = function(head, left, right) {
+    // 定义一个哨兵节点
+    let dummy = {
+        next:head
+    }
+    let temp =dummy
+    for (let i = 0; i <left-1;i++){
+        temp =temp.next  //找到left的节点
+    }
+    let prev = temp.next; //left 的下个节点
+    let cur = prev.next; //lef的下下节点
+    // 找到区间
+    for (let j = 0; j < right-left; j++){
+        let next = cur.next
+        cur.next = prev
+        prev = cur
+        cur = next
+    }
+    temp.next.next = cur
+    temp.next = prev
+    return dummy.next
+
 
 };
 // @lc code=end
