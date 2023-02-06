60c87e42ebc7a69c8439f70f14a45a5ae88ec7b1
张艳华 <939593210@qq.com>
2023-02-06 23:30:23 +0800

    feat: yargs 的基本使用

--- 脚手架/mo-test/bin/index.js
+++ 脚手架/mo-test/bin/index.js
@@ -1,16 +1,34 @@
 #!/usr/bin/env node
-const utils = require("./utils");
-const argv = require("process").argv;
-const command = argv[2];
-const options = argv.slice(3);
-let [option, param] = options;
-option = option && option.replace(/^-+/, "");
-if (command) {
-  console.log("执行命令", command);
-  if (utils[command]) {
-    utils[command]({ option, param });
-  }
-} else {
-  console.log("请输入命令");
-}
-//实现参数解析
+console.log("action");
+
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const arg = hideBin(process.argv);
+const cli = yargs(arg);
+const dedent = require("dedent");
+yargs(arg)
+  .usage("mo-test [command] <options>")
+  .strict()
+  .alias("h", "help")
+  .alias("v", "version")
+  .wrap(cli.terminalWidth()) //宽度
+  .epilogue(dedent`这里可以写一些说明`)
+  .options({
+    debug: {
+      type: "boolean",
+      describe: "'debug'模式",
+      alias: "d",
+    },
+  })
+  .option("ci", {
+    type: "boolean",
+    describe: "是否是CI环境",
+    alias: "c",
+  })
+  .option("register", {
+    type: "boolean",
+    describe: "register",
+    alias: "r",
+  })
+  .group(["debug", "ci"], "调试选项:")
+  .demandCommand(1, "最少输入一个参数").argv;
