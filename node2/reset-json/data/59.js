2a86e9105a823d512489b85cf43455cbf8382ca8
张艳华 <939593210@qq.com>
2023-02-10 16:34:07 +0800

    feat: 去掉一些没必要的

--- node2/reset-json/data/59.js
+++
@@ -1,7 +0,0 @@
-export default {
-  title: '一图读懂：《金山区推进经济数字化转型行动计划（2022-2024年）》',
-  createTime: '2022年8月17日',
-  content: `<img src="http://220.196.242.107/qrcode/location/static/img/js59/1.1.png" style="width: 100%; height: 100%;display: block;"  />
-  <img src="http://220.196.242.107/qrcode/location/static/img/js59/1.2.png" style="width: 100%; height: 100%;display: block;margin-top: -1px"  />
-  `
-};
